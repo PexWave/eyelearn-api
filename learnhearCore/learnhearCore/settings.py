d84439a3882f6c680df675655dfc588d89ae17b0
pexwave <asakilsarhan@gmail.com>
2024-02-16 23:44:55 +0800

    update

--- learnhearCore/learnhearCore/settings.py
+++ learnhearCore/learnhearCore/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = 'django-insecure-*+y=j$vtfjk^2s)_wfpv3pvvhty5y5-vf911)vy!u^iusqe4!*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG')
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
+
 
 ALLOWED_HOSTS = ['*']
 
